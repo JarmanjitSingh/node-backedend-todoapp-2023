433a8db92f35d0e0d3c1fc7fc841c2a93c2e5684
JarmanjitSingh <jarmanjits176@gmail.com>
2023-09-24 02:36:21 +0530

    cors placed above the routes

--- app.js
+++ app.js
@@ -11,16 +11,17 @@ export const app = express();
 
 //using middlewares
 app.use(express.json()); //for accessing req.body via postman and always use first before use routes
-app.use(cookieParser())
-
-//using routes
-app.use("/api/v1/users", userRouter); // using the user router because with user router we will able to add prifix of url
-app.use("/api/v1/task", taskRouter)
+app.use(cookieParser());
 app.use(cors({
   origin: [process.env.FRONTEND_URL],
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true //with true you can send headers or cookies to frontend otherwise api will work but you dont able to set cookie and headers to the frontend. and withCredentials frontend te true krna.
 }))
+
+//using routes
+app.use("/api/v1/users", userRouter); // using the user router because with user router we will able to add prifix of url
+app.use("/api/v1/task", taskRouter)
+
 
 
 
